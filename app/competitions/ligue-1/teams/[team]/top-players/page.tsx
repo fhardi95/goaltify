a6fc7871cdcebb7ad77d29f903b25b9f8feae6c0
Fhardi Said <fhardi.said@gmail.com>
2026-05-11 22:34:09 +0100

    fix: team pages routing

---
+++ app/competitions/ligue-1/teams/[team]/top-players/page.tsx
@@ -0,0 +1,1 @@
+export { default, generateMetadata, generateStaticParams } from '@/app/competitions/[competition]/teams/[team]/top-players/page'
---
+++ app/competitions/premier-league/teams/[team]/page.tsx
@@ -0,0 +1,1 @@
+export { default, generateMetadata, generateStaticParams } from '@/app/competitions/[competition]/teams/[team]/page'
